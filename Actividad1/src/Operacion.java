c4a85f364f548102bd76228fb3ddfd24bb272867
Alejoflorez27 <101139306+Alejoflorez27@users.noreply.github.com>
2022-08-28 13:14:13 -0500

    Update Operacion.java

--- Actividad1/src/Operacion.java
+++ Actividad1/src/Operacion.java
@@ -1,6 +1,7 @@
 import javax.swing.JOptionPane;
 public class Operacion {
 	
+	//Resive como argumento el objeto de persona y se realiza el calculo del IMC
 	public void CalculoImc(Persona per) {
 		double result = per.getPeso()/(per.getTalla()*per.getTalla());
 		JOptionPane.showMessageDialog(null, "El IMC de la persona "+ per.nombre+" es: "+result);
